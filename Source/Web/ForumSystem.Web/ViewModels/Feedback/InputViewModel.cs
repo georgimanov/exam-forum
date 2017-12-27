d76222476bc255a062a76c7b31a205ca89aec7b8
Georgi Manov <georgimanov@gmail.com>
2017-12-27 22:29:05 +0200

    added create form for feedback

---
+++ Source/Web/ForumSystem.Web/ViewModels/Feedback/InputViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ForumSystem.Web.ViewModels.Feedback
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class InputViewModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+    }
+}
