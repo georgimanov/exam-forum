2266c919a9329d372f534cf53d99dffc28eb7d95
Georgi Manov <georgimanov@gmail.com>
2017-12-27 17:32:00 +0200

    seeded posts and tags

--- Source/Data/ForumSystem.Data/Migrations/Configuration.cs
+++ Source/Data/ForumSystem.Data/Migrations/Configuration.cs
@@ -1,9 +1,10 @@
 namespace ForumSystem.Data.Migrations
 {
-    using System;
-    using System.Data.Entity;
+    using System.Collections.Generic;
     using System.Data.Entity.Migrations;
     using System.Linq;
+
+    using ForumSystem.Data.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<ApplicationDbContext>
     {
@@ -17,6 +18,30 @@ namespace ForumSystem.Data.Migrations
 
         protected override void Seed(ApplicationDbContext context)
         {
+            if (!context.Tags.Any())
+            {
+                var tagList = new List<Tag>();
+                for (int i = 0; i < 20; i++)
+                {
+                    var tag = new Tag {Name = $"Tag {i}"};
+                    context.Tags.Add(tag);
+                    tagList.Add(tag);
+                }
+
+                var tagIndex = 0;
+                for (int i = 0; i < 20; i++ )
+                {
+                    var post = new Post { Content = $"Post content {i}", Title = $"Title {i}" };
+                    context.Posts.Add(post);
+                    for (int j = 0; j < 5; j++)
+                    {
+                        post.Tags.Add(tagList[tagIndex % tagList.Count]);
+                        tagIndex++;
+                    }
+                }
+
+                context.SaveChanges();
+            }
         }
     }
 }
