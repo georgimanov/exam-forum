30d2a30bc554cd4b4bc01802eab48d0e4691670e
Georgi Manov <georgimanov@gmail.com>
2017-12-27 22:10:45 +0200

    added Feedback controller and view

---
+++ Source/Web/ForumSystem.Web/Controllers/FeedbackController.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class FeedbackController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
