5ec58e57fa8b029b684f600fc40b594be6bf197c
Georgi Manov <georgimanov@gmail.com>
2017-12-27 22:49:41 +0200

    added create for feedbacks

--- Source/Web/ForumSystem.Web/Controllers/FeedbackController.cs
+++ Source/Web/ForumSystem.Web/Controllers/FeedbackController.cs
@@ -1,13 +1,53 @@
 ﻿namespace ForumSystem.Web.Controllers
 {
     using System.Web.Mvc;
+    using Microsoft.AspNet.Identity;
+
+    using ForumSystem.Data.Common.Repository;
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.ViewModels.Feedback;
 
     public class FeedbackController : Controller
     {
+        private readonly IDeletableEntityRepository<Feedback> _feedbacks;
+
+        public FeedbackController(IDeletableEntityRepository<Feedback> feedbacks)
+        {
+            this._feedbacks = feedbacks;
+        }
+
         [HttpGet]
         public ActionResult Create()
         {
             return this.View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(InputViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var feedback = new Feedback
+            {
+                Content = model.Content,
+                Title = model.Title
+            };
+
+            if (this.User.Identity.IsAuthenticated)
+            {
+                feedback.AuthorId = this.User.Identity.GetUserId();
+            }
+
+            this._feedbacks.Add(feedback);
+            this._feedbacks.SaveChanges();
+
+            this.TempData["Notification"] = "Feedback created!";
+
+            return this.View("/"); 
         }
     }
 }
