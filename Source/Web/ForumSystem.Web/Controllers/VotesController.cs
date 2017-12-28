d52e653b7aaa502295f9f0916a95c06961d8bc64
Georgi Manov <georgimanov@gmail.com>
2017-12-28 02:48:55 +0200

    implemented voting with AJAX

---
+++ Source/Web/ForumSystem.Web/Controllers/VotesController.cs
@@ -0,0 +1,62 @@
+﻿namespace ForumSystem.Web.Controllers
+{
+    using Microsoft.AspNet.Identity;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using ForumSystem.Data.Common.Repository;
+    using ForumSystem.Data.Models;
+
+    [Authorize]
+    public class VotesController : Controller
+    {
+        private readonly IRepository<PostVote> _votes;
+
+        public VotesController(IRepository<PostVote> votes)
+        {
+            this._votes = votes;
+        }
+
+        [HttpPost]
+        public ActionResult Vote(int postId, int voteType)
+        {
+            if (voteType > 1)
+            {
+                voteType = 1;
+            }
+
+            if (voteType < -1)
+            {
+                voteType = -1;
+            }
+
+            var userId = this.User.Identity.GetUserId();
+
+            var vote = this._votes
+                .All()
+                .FirstOrDefault(x => x.AuthorId == userId && x.PostId == postId);
+
+            if (vote == null)
+            {
+                vote = new PostVote
+                {
+                    AuthorId = userId,
+                    PostId = postId,
+                    Type = (VoteType)voteType
+                };
+
+                this._votes.Add(vote);
+            }
+            else
+            {
+                vote.Type = (VoteType)voteType;
+            }
+
+            this._votes.SaveChanges();
+
+            var postVotes = this._votes.All().Where(x => x.PostId == postId).Sum(x => (int)x.Type);
+
+            return this.Json(new { Count = postVotes });
+        }
+    }
+}
--- Source/Web/ForumSystem.Web/ViewModels/Home/IndexBlogPostViewModel.cs
+++ Source/Web/ForumSystem.Web/ViewModels/Home/IndexBlogPostViewModel.cs
@@ -1,18 +1,31 @@
 ﻿namespace ForumSystem.Web.ViewModels.Home
 {
     using System.Collections.Generic;
+    using System.Linq;
+
+    using AutoMapper;
 
     using ForumSystem.Data.Models;
     using ForumSystem.Web.Infrastructure.Mapping;
 
-    public class IndexBlogPostViewModel : IMapFrom<Post>
+    public class IndexBlogPostViewModel : IMapFrom<Post>, IHaveCustomMappings
     {
         public int Id { get; set; }
 
         public string Title { get; set; }
 
+        public int VotesCount { get; set; }
+
         public IEnumerable<IndexBlogPostTagViewModel> Tags { get; set; }
 
         public string Url => $"questions/{this.Id}/{this.Title.ToLower().Replace(" ", "-")}";
+
+        public void CreateMappings(IConfiguration configuration)
+        {
+            configuration.CreateMap<Post, IndexBlogPostViewModel>()
+                .ForMember(
+                    x => x.VotesCount, 
+                    opt => opt.MapFrom(x => x.Votes.Any() ? x.Votes.Sum(v => (int) v.Type) : 0));
+        }
     }
 }
