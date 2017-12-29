bce5015ed138e9c8884c61914d4eadfe61af1ba8
Georgi Manov <georgimanov@gmail.com>
2017-12-29 17:46:46 +0200

    added posts adminitration

--- Source/Web/ForumSystem.Web/Areas/Administration/Controllers/PostsController.cs
+++ Source/Web/ForumSystem.Web/Areas/Administration/Controllers/PostsController.cs
@@ -1,16 +1,109 @@
-﻿using System.Web.Mvc;
-
-using Kendo;
-using Kendo.Mvc;
-
-namespace ForumSystem.Web.Areas.Administration.Controllers
+﻿namespace ForumSystem.Web.Areas.Administration.Controllers
 {
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Microsoft.AspNet.Identity;
+
+    using AutoMapper.QueryableExtensions;
+    using Kendo;
+    using Kendo.Mvc;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+
+    using ForumSystem.Data.Common.Repository;
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Areas.Administration.ViewModels;
+    using ForumSystem.Web.Infrastructure;
+
     public class PostsController : Controller
     {
-        // GET: Administration/Posts
+        private readonly IDeletableEntityRepository<Post> _posts;
+        private readonly ISanitizer _sanitizer;
+
+        public PostsController(IDeletableEntityRepository<Post> posts, ISanitizer sanitizer)
+        {
+            _posts = posts;
+            _sanitizer = sanitizer;
+
+        }
+
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Read([DataSourceRequest]DataSourceRequest request, PostsViewModel model)
+        {
+            var commentsViewModel = this._posts
+                .All()
+                .Project()
+                .To<PostsViewModel>()
+                .ToList()
+                .ToDataSourceResult(request);
+
+            return this.Json(commentsViewModel, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Create([DataSourceRequest]DataSourceRequest request, PostsViewModel model)
+        {
+            if (this.ModelState.IsValid && model != null)
+            {
+                var post = new Post
+                {
+                    Title = model.Title,
+                    Content = this._sanitizer.Sanitize(model.Content),
+                };
+
+                if (this.User.Identity.IsAuthenticated)
+                {
+                    post.AuthorId = this.User.Identity.GetUserId();
+                }
+
+                this._posts.Add(post);
+                this._posts.SaveChanges();
+            }
+
+            return this.GetGridOperations(request, model);
+        }
+
+        [HttpPost]
+        public ActionResult Update([DataSourceRequest]DataSourceRequest request, PostsViewModel model)
+        {
+            if (this.ModelState.IsValid && model != null)
+            {
+                var post = _posts.GetById(model.Id);
+                post.Title = model.Title;
+                post.Content = model.Content;
+                post.ModifiedOn = DateTime.UtcNow;
+
+                if (this.User.Identity.IsAuthenticated)
+                {
+                    post.AuthorId = this.User.Identity.GetUserId();
+                }
+
+                _posts.Update(post);
+                _posts.SaveChanges();
+            }
+
+            return this.GetGridOperations(request, model);
+        }
+
+        [HttpPost]
+        public ActionResult Destroy([DataSourceRequest]DataSourceRequest request, PostsViewModel model)
+        {
+            this._posts.Delete(model.Id);
+            this._posts.SaveChanges();
+
+            return this.GetGridOperations(request, model);
+        }
+
+        protected JsonResult GetGridOperations([DataSourceRequest]DataSourceRequest request, PostsViewModel model)
+        {
+            return this.Json(new[] { model }.ToDataSourceResult(request, this.ModelState));
         }
     }
 }
---
+++ Source/Web/ForumSystem.Web/Areas/Administration/ViewModels/PostsViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace ForumSystem.Web.Areas.Administration.ViewModels
+{
+    using System;
+
+    using ForumSystem.Data.Models;
+    using ForumSystem.Web.Infrastructure.Mapping;
+
+    public class PostsViewModel : IMapFrom<Post>
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
