ba6315af9d884275bf32208dacc4b89e4137f958
Georgi Manov <georgimanov@gmail.com>
2017-12-29 18:01:14 +0200

    added DataAttributes to PostViewModel

--- Source/Web/ForumSystem.Web/Areas/Administration/ViewModels/PostsViewModel.cs
+++ Source/Web/ForumSystem.Web/Areas/Administration/ViewModels/PostsViewModel.cs
@@ -1,14 +1,17 @@
 ﻿namespace ForumSystem.Web.Areas.Administration.ViewModels
 {
     using System;
+    using System.ComponentModel.DataAnnotations;
 
     using ForumSystem.Data.Models;
     using ForumSystem.Web.Infrastructure.Mapping;
 
     public class PostsViewModel : IMapFrom<Post>
     {
+        [Key]
         public int Id { get; set; }
 
+        [MaxLength(100)]
         public string Title { get; set; }
 
         public string Content { get; set; }
